b0b6e8a360ed76f5c585e2702b6f90716006013b
mac <2582909040@qq.com>
2022-06-28 19:15:28 +0800

    ok

---
+++ src/ble.net.sampleapp-android/MtuService.cs
@@ -0,0 +1,15 @@
+//using System;
+//using ble.net.sampleapp.BleMesh;
+//using Android.Bluetooth;
+//namespace ble.net.sampleapp.android
+//{
+//   public class MtuService:IMtuService
+//   { 
+//      public void GetMtu(int MTU)
+//      {
+//         BluetoothGatt bluetoothGatt = new BluetoothGatt(n); ;
+//         bluetoothGatt.RequestMtu(MTU);
+
+//      }
+//   }
+//}
